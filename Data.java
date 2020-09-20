07aba2ae4c33a6138a13303cb5cd364b2edf036f
Vaishnavi Ganji <52523491+VaishnaviGanji@users.noreply.github.com>
2020-09-20 11:02:49 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Data {
+	public static HashMap<Integer, String> customers= new HashMap<>();
+	public static void main(String[] args) {
+		customers.put(1, "Vaishnavi");
+		customers.put(2, "Manoj");
+	}
+}
