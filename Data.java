552878ac408442cacea3fb74806bf579cecf2b14
Vaishnavi Ganji <52523491+VaishnaviGanji@users.noreply.github.com>
2020-09-20 11:09:54 +0530

    Delete Data.java

--- Data.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Data {
-	public static HashMap<Integer, String> customers= new HashMap<>();
-	public static void main(String[] args) {
-		customers.put(1, "Vaishnavi");
-		customers.put(2, "Manoj");
-	}
-}
